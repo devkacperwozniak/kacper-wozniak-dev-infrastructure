f2b92a3920c8e1c3e232c3d918933123117833d5
Kacper Wozniak <dev.kacper.wozniak@gmail.com>
2025-02-23 14:48:39 +0100

    change workflow

--- vpc.tf
+++ vpc.tf
@@ -60,3 +60,4 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   security_group_id = aws_security_group.allow_tls.id
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1"
+}
