023a3612643bcaa109953a89216ed9ffd6466f47
Kacper Wozniak <dev.kacper.wozniak@gmail.com>
2025-02-23 19:09:53 +0100

    change sg

--- vpc.tf
+++ vpc.tf
@@ -32,32 +32,25 @@ resource "aws_security_group" "aws_fargate_sg" {
   tags = {
     Name = "aws_fargate_sg"
   }
-}
 
-resource "aws_vpc_security_group_ingress_rule" "aws_fargate_sg_ingress_http" {
-  security_group_id = aws_security_group.aws_fargate_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "-1"
-  to_port           = 80
-}
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_vpc_security_group_ingress_rule" "aws_fargate_sg_ingress_https" {
-  security_group_id = aws_security_group.aws_fargate_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  ip_protocol       = "-1"
-  to_port           = 443
-}
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.aws_fargate_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.aws_fargate_sg.id
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
